5f170010ffe4f9136c3f678936f34f476ce8dc57
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2023-01-29 02:29:16 +0600

    mongodb added

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ app.get("/", (req, res) => {
   res.send("Power Hack server is running successfully");
 });
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@power-hack.svkllsd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log("DB connected successfully.");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+dbConnect();
+
 app.listen(port, () => {
   console.log(`Power Hack server server is running on port ${port}`);
 });
