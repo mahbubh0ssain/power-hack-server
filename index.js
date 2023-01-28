6120186a7f2b8db1114892656b43ef5551782549
Md. Mahbub Hossain <mahbubh0ssain.dev@gmail.com>
2023-01-29 04:57:50 +0600

    user registed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 app = express();
@@ -28,6 +29,32 @@ const dbConnect = async () => {
   }
 };
 dbConnect();
+
+const UsersCollection = client.db("Power-Hack").collection("usersCollection");
+
+//user registration
+app.put("/registration", async (req, res) => {
+  try {
+    const user = req.body;
+    const result = await UsersCollection.updateOne(
+      { email: req?.body?.email },
+      { $set: user },
+      { upsert: true }
+    );
+    const token = jwt.sign(user.email, process.env.ACCESS_TOKEN);
+
+    res.send({
+      success: true,
+      result,
+      token,
+    });
+  } catch (err) {
+    res.send({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Power Hack server server is running on port ${port}`);
